8fd66990f49d1757ee81df58a81fe2fd046dcbb6
Nabin Sharma <nabin.s.sharma@gmail.com>
2013-05-15 00:13:01 -0400

    consolidate Tweepy_Hashtags_1 and 2 with few nits
      read symplur hashtags from json
      add couple of helper functions for more clarity

--- Tweepy_Hashtags_2.py
+++ Tweepy_Hashtags_2.py
@@ -1,4 +1,11 @@
-#Read About tweepy at http://pythoncentral.org/introduction-to-tweepy-twitter-for-python/
+"""Read About tweepy at
+http://pythoncentral.org/introduction-to-tweepy-twitter-for-python/
+
+Todos:
+  * Options for both stdout anf file streaming.
+"""
+
+from collections import namedtuple
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
@@ -6,20 +13,31 @@ from tweepy import Stream
 import json
 import tweepy
 
-config_json = open('config.json', 'r')
-config = json.load(config_json)
+TWITTER_CONFIGS = 'config.json'
+MEDICAL_HASHTAGS = 'medical_hashtags.json'
 
-print 
 
-# Go to http://dev.twitter.com and create an app.
-# The consumer key and secret will be generated for you after
-consumer_key    = config["consumer_key"]
-consumer_secret = config["consumer_secret"]
+def get_twitte_configs():
+    config = json.load(open(TWITTER_CONFIGS, 'r'))
+    twitter_configs = namedtuple(
+        'TwitterConfigs',
+        'consumer_key, consumer_secret, access_token, access_token_secret')
 
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
-access_token = config["access_token"]
-access_token_secret = config["access_token_secret"]
+    # Go to http://dev.twitter.com and create an app.
+    # The consumer key and secret will be generated for you.
+    twitter_configs.consumer_key = config["consumer_key"]
+    twitter_configs.consumer_secret = config["consumer_secret"]
+    
+    # After the step above, you will be redirected to your app's page.
+    # Create an access token under the the "Your access token" section.
+    twitter_configs.access_token = config["access_token"]
+    twitter_configs.access_token_secret = config["access_token_secret"]
+    return twitter_configs
+
+
+def get_medical_hashtags():
+    medical_hashtags = json.load(open(MEDICAL_HASHTAGS, 'r'))
+    return medical_hashtags['symplur']
 
 
 class StdOutListener(StreamListener):
@@ -29,7 +47,8 @@ class StdOutListener(StreamListener):
         # Prints the text of the tweet
 
         print('Tweet text: ' + status.text.encode('utf-8'))
-        directAttrs = 'place,coordinates,lang,created_at,retweeted_status,source source_url'.split(',')
+        directAttrs = ('place,coordinates,lang,created_at,'
+                       'retweeted_status,source source_url').split(',')
         for k in directAttrs:
             value = getattr(status, k, None)
             if value is not None:
@@ -50,17 +69,16 @@ class StdOutListener(StreamListener):
         print('Timeout...')
         return True  # To continue listening
 
+
 if __name__ == '__main__':
     listener = StdOutListener()
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    trackList = ['#krankenhaus','#krebs','#kyhc','#laruletadelahorro','#Lasik','#LcChat','#LHH_H2H','#LHI','#LHIN','#lifestylemedicine','#LivingwithMS','#LLABagLadies','#longtermcare','#LTC','#LupusStyle','#malpractice','#mammogram','#mammogram','#mammograms','#mammography','#massagetherapy','#maternalHealth','#maternalMortality','#MayoCIM','#mayoclinic','#mbmed','#mcaidx','#mccfi','#mccsm','#McrFluSafe','#McrMentalHealth','#McrSCR','#MDchat','#MDehr','#MDigitalLife','#MDMLG','#MeaningfulUse','#meatlessmonday','#mecfs','#medApps','#medcomms','#MedDevice','#meded2','#Medicaid','#medical','#medicalbilling','#medicalhome','#medicalimaging','#MedicalMonday','#Medicare','#medicine','#medizin','#MedSafety','#medschool','#medschools','#MedSM','#MedStartr','#medstudent','#MedsUpAway','#medtech','#MeduTOT','#MedXeP','#mefirst','#mentalhealth','#MentalHealthMatters','#mentalhealthmonday','#mentalhealthmonth','#mentalillness','#mgma','#mh4ot','#mhbrain','#mHealth','#mHealthChat','#mHealthZone','#mhm','#mhoz','#midwife','#midwifery','#midwives','#milhealth','#MindfulEating','#miscarriage','#mobilehealth','#modelH','#Montefiore','#Mountaintrauma','#MRI','#MU2','#MyHealth','#myhealthstory','#myplate','#NAPH','#naturalhealth','#NaturalMedicine','#Naturopathic','#NCDchat','#neonatologist','#neonatology','#NephMadness','#Nephrologist','#Nephrology','#Neurologist','#Neurologists','#Neurology','#neuroscience','#Neurosurgeon','#Neurosurgeons','#Neurosurgery','#ngdoc','#nhis','#nhs','#NHSchange','#nhschangeday','#nhsreform','#nhsreforms','#NHSTweets','#NHSXmas','#NIH','#NIVW','#NMAM','#NNM','#novoiceweek','#NowTrending2012','#NPchat','#NPfIT','#NPINchat','#NUR325','#NurFlash','#nurse','#nurses','#nurseshift','#NurseUp','#nursing','#nursinghome','#nurstudent','#nutrition','#NYCHBL','#obgyn','#Obstetrics','#Occhat','#occmatters','#occtherapy','#occupationaltherapy','#occupyhealth','#occupyhealthcare','#od12','#ONC','#ONC','#oncochat','#Oncologist','#Oncologists','#Oncology','#OneLineOneDay','#openEHR','#opengpsoc','#openMRS','#Ophthalmology','#optometry','#OralCancer','#oralhygiene','#OralSurgery','#organdonation','#Orthodontics','#Orthopaedics','#orthopedics','#otolaryngologists','#Otolaryngology','#OTuesday','#ovarianchat','#P2PHC','#PAGuidelines','#pahohealth4change','#palliative','#PALLIchat','#pancreas','#ParaAus','#paramedic','#ParamedicABC','#PartD','#Pathologist','#Pathology','#patient','#patienteducation','#patientengagement','#patientexperience','#PatientNav','#patients','#pbschat','#PCMH','#PCORI','#Pediatric','#Pediatrician','#Pediatricians','#Pediatrics','#pedpc','#pedsurg','#peremendemovement','#Pflege','#pharma','#pharma100','#pharmaceutical','#pharmaceuticals','#pharmacist','#pharmacy','#pharmacynews','#pharmamktg','#pharmaphorum','#phealth','#PHOAMed','#phr','#PHSSR','#phychat','#physicaltherapist','#physicaltherapy','#physician','#physicianassistant','#PhysicianAssistants','#physicians','#PlasticSurgeon','#PlasticSurgeons','#PlasticSurgery','#pm101','#Podiatrist','#Podiatrists','#Podiatry','#poliochat','#PopulationHealth','#PPACA','#practicemanagement','#pregnancychat','#PreSLP','#prevwell','#PrimaryCare','#prostatecare','#protesisdental','#ProtonTherapy','#psych','#Psychiatrist','#Psychiatrists','#Psychiatry','#Psychologie','#Psychologist','#Psychologists','#Psychology','#ptcentcare','#pted','#ptexperience','#PToutcomes','#ptsafety','#ptsafetychat','#ptspkr','#pubhealth','#publichealth','#Pulmonary','#puwebcast','#Qczv','#qic','#QSWB','#QuantifiedSelf','#R2Hchat','#Radiologist','#Radiologists','#Radiology','#RaganSocial','#rareACTION','#RD2Be','#readmissions','#rebif','#recoverychat','#refugeehealth','#rehab','#rehabchat','#rehabilitation','#respirarbien','#respiratory','#respiratorytherapist','#revcycle','#Rheumatologist','#Rheumatology','#rheumedu','#rhumedu','#righttoavoice','#rimasanestesicas','#RN','#RNedu','#ruralhealth','#RuralMed','#RuralMH','#RWJFWeb20','#rxcom','#rxsave','#RXSocial','#s4pm','#sacom','#salu20','#SaudiMedEd','#SaveAbx','#SaveAll','#SCFitFridays','#sdm','#SDMchat','#SDOH','#SDOHchat','#secondvictim','#SegPac','#senior','#seniors','#sensibilidaddental','#sensorychat','#SGReport','#shoyomo','#signagainststroke','#singlepayer','#skincare','#SleepDeprivation','#slp','#SLP2B','#slpchat','#slpeeps','#SLPeeps','#smccg','#sochealth','#socialQI','#SoMeCME','#spedchat','#spine','#Spital','#spoonie','#spoonies','#sportsmedicine','#startuphealth','#stemcell','#strokeoz','#StTwitters','#SubstanceAbuse','#supertwision','#surgeon','#svmed','#SW4HC','#SWLIP','#talkalz','#talkingmentalhealth','#TCK','#TDWI','#teeth','#telecare','#telehealth','#telemedicine','#thcsm','#therapist','#therapy','#therightcare','#thetreasurehunt','#thewalkinggallery','#ThinkIBC','#thyroid','#ticsalut','#transfusion','#Transplantation','#traumaresearch','#tsunamiblanco','#TT4health','#tvlmed','#tweEDers','#twitjc','#UCLAMDChat','#UICC','#ukcare','#ultrasound','#UMCHOP','#UPIM','#urgentcare','#urojc','#Urologist','#Urology','#USPSTF','#vaccination','#vaccines','#VAResearch','#VAResearchWK','#Vasectomy','#vaxfax','#vdgm','#VetoViolence','#VitalSigns','#VMMC','#vschat','#wcphep','#WDD','#WDHD','#wellocracy','#wemidwives','#whatifhc','#whcc','#wheelchair','#wholify','#WhyStopNow','#wirelesshealth','#Withings','#wmnhealth']
+    twitter_configs = get_twitte_configs()
+    auth = tweepy.OAuthHandler(twitter_configs.consumer_key,
+                               twitter_configs.consumer_secret)
+    auth.set_access_token(twitter_configs.access_token,
+                          twitter_configs.access_token_secret)
+    
     stream = Stream(auth, listener)
-    stream.filter(track = trackList)
-
-
-'''
-    for attr, value in status.__dict__.iteritems():
-        print attr, ": ", value
-'''
+    tracklists = get_medical_hashtags()
+    for tracklist in tracklists:
+        stream.filter(track = tracklist)
