53735fea52c38c3b4e1154feec92880ed6339ba8
Juan Ignacio Gil Gómez <iwan.manjak@gmail.com>
2014-07-27 16:19:11 +0200

    Taking into account that a tweet could be represented in different files, if it refers to more than one disease

--- send_tweets_to_mongodb.py
+++ send_tweets_to_mongodb.py
@@ -28,30 +28,38 @@ from functools import partial
 
 def process_disease_file(path,group,collection,file):
     
-
+    ni=0
+    nu=0
     wholepath=path+'/'+group+'/'+file
     with open(wholepath,'r') as disease_file:
         #Read the tweet file as a dictionary
         tweets=csv.DictReader(disease_file)
         disease=file[7:-4]
-        print disease
     
         #Adding the group and disease fields to all tweets
         for thistweet in tweets:
-            thistweet['group']=group
-            thistweet['disease']=disease
+            thistweet['group']=[group]
+            thistweet['disease']=[disease]
             
             #Is this tweet in the database?
-            if collection.find({"url": thistweet['url']}).count()==0:
+            tweetindb=collection.find_one({"url": thistweet['url']})
+            if tweetindb==None:
 
                 #Insert tweet in the database
+                ni+=1
                 thistweet_id = collection.insert(thistweet)
 
             else:
-                print 'Tweet '+repr(thistweet['url'])+' already in the database'
+                #Maybe the tweet was in the database for other disease file
+                nu+=1
+                tweetindb['group']=list(set(tweetindb['group']+group))
+                tweetindb['disease']=list(set(tweetindb['disease']+disease))
+                mycollection.update({'_id':tweetindb['_id']}, {"$set": tweetindb}, upsert=False)
+
 
     
     disease_file.close()
+    print repr(disease)+': '+repr(ni)+' inserted, '+repr(nu)+' updated'
 
 
 
@@ -66,11 +74,12 @@ if __name__ == '__main__':
     #Database
     client = MongoClient()
     db = client['HealthCare_Twitter_Analysis']
+    #collection = db.tweets
     
     #Use test database for debugging
     #db = client['test']
     
-    #collection = db.tweets
+    
 
 
     #Navigate directory structure
@@ -80,9 +89,7 @@ if __name__ == '__main__':
             partial_process_disease_file=partial(process_disease_file,path,g,db.tweets)
             pool.map(partial_process_disease_file,files)
             pool.close
-            pool.join
-                #pool.map(process_disease_file(path,group,file,collection),)
-                #process_disease_file(path,group,file,collection)
+
         except:
                 continue
 
