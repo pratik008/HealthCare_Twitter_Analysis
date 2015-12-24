ffe2b38dd06b7f5e26ce742ab84070f79a8a86c0
bhushan_shah <bhushan.shah@stonybrook.edu>
2015-12-24 15:07:58 -0800

    Stream tweets having specific tags and store to external file; (Credentials are fake)

---
+++ Stream/src/utility.py
@@ -0,0 +1,10 @@
+
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key): byteify(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
